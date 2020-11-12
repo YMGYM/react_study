8b358ecd066f057591b42d028327027b24f07e10
YMGYM <good_day1234@naver.com>
2020-11-12 22:04:47 +0900

    20201112

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {Component} from 'react';
+// 가상돔
+import Virtual from './components/Virtual';
+
+
+// 객체형 호출(함수형 호출보다 편리)
+class App extends Component{
+  //이벤트 핸들러
+  //state(초기값 설정 등)
+
+  //JSX 템플릿
+  render(){
+    return(
+      // view에 보여지는 내용
+      // 메인 돔임
+      <div>
+        <h3>참돔이에요</h3>     
+        <Virtual></Virtual>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Virtual.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './Virtual.css';
+
+class Virtual extends Component{
+    render(){
+        return(
+            <h4>가상 돔이에요 ㅎㅎ</h4>
+        );
+    }
+}
+
+export default Virtual;
