aad3a2c23064c2fd5187f3a291889743ad9ef356
YMGYM <good_day1234@naver.com>
2020-11-16 21:23:49 +0900

    20201116

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 import React, {Component} from 'react';
@@ -10,6 +10,28 @@ import Virtual from './components/Virtual';
 class App extends Component{
   //이벤트 핸들러
   //state(초기값 설정 등)
+  state = {
+    value : ''
+  }
+  // 이건 안바뀜 (불변성)
+
+  handleChange = (e) => {
+    this.setState({
+      value: e.target.value
+    })
+  }
+  //es6문법 (함수 선언법)
+  //리액트에선 function을 선언할 수 없다.
+  //e = event의 약어
+  //state 값을 변경하고 싶을 때는 setState로변경해야함 - 불변성 보장
+
+
+  handleDestroy = () => {
+    this.setState({
+      value: ''
+    })
+  }
+  // 인자를 안받아도 됨.
 
   //JSX 템플릿
   render(){
@@ -18,10 +40,29 @@ class App extends Component{
       // 메인 돔임
       <div>
         <h3>참돔이에요</h3>     
-        <Virtual></Virtual>
+        <Virtual handleChange={this.handleChange} handleDestroy={this.handleDestroy} value={this.state.value}></Virtual>
+        <label>{this.state.value}</label>
       </div>
     );
   }
 }
 
 export default App;
+
+
+// rcc => 클래스형 : 조금 무거운 대신 쓸 수 있는 기능이 많다.(라이프사이클..) rfcp => 함수형 : 클래스형의 반대.
+
+// props : properties - 부모 자식간의 데이터바인딩이 이루어진다.
+// state : 현재 상태
+// state 조작
+
+// 클래스형일때.. 
+// constructor(props){
+//   super(props);
+//   this.state = {초기값}
+// }
+
+// 이걸 줄여서...
+// state = {
+
+// }
--- src/components/Virtual.js
+++ src/components/Virtual.js
@@ -1,12 +1,36 @@
-import React, {Component} from 'react';
-import './Virtual.css';
+// import React, {Component} from 'react';
+// import './Virtual.css';
 
-class Virtual extends Component{
-    render(){
-        return(
+// class Virtual extends Component{
+//     render(){
+//         return(
+//             <div>
+//                 <h4>가상 돔이에요 ㅎㅎ</h4>
+//                 <input type="text" onChange={this.props.handleChange} value={this.props.value}></input>
+//                 <button onClick={this.props.handleDestroy}>삭제</button>
+//             </div>
+            
+//         );
+//     }
+// }
+
+// export default Virtual;
+// ---------- 함수형
+import React from 'react';
+
+
+const Virtual = ({value, handleChange, handleDestroy}) => {
+    // 함수형에서는 hook을 사용해야 state 를 쓸 수 있음
+    return (
+        <div>
             <h4>가상 돔이에요 ㅎㅎ</h4>
-        );
-    }
-}
+            <input type="text" onChange={handleChange} value={value}></input>
+            <button onClick={handleDestroy}>삭제</button>
+            <label>{this.state.value}</label>
+        </div>
+    );
+};
 
+
+// 메인돔은 클래스형, 가상돔은 함수형을 추천
 export default Virtual;
